0bb6b3424fa723e356d3454f26d2c6d5dbbf6661
yuhan <5b3g0001@stust.edu.tw>
2026-09-20 17:55:02 +0800

    加入專案檔案。

---
+++ 2026_WpfApp1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2026_WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
